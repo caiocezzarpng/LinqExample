a8f4afa8bedb44b2ac8638d200e21856d409d1c9
Caio Cesar Sousa Bandeira <caio.bandeira@fitbank.com.br>
2023-08-21 10:59:41 -0300

    Add project files.

---
+++ LinqExample/Program.cs
@@ -0,0 +1,53 @@
+﻿using LinqExample.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\in.csv";
+            Console.Write("Enter salary: ");
+            double limit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            List<Employee> employees = new List<Employee>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream) {
+                        string[] fields = sr.ReadLine().Split(',');
+                        string name = fields[0];
+                        string email = fields[1];
+                        double salary = double.Parse(fields[2], CultureInfo.InvariantCulture);
+                        employees.Add(new Employee(name, email, salary));
+                    }
+
+                    var emails = employees.Where(obj => obj.Salary > limit).OrderBy(obj => obj.Email).Select(obj => obj.Email);
+
+                    var sum = employees.Where(obj => obj.Name[0] == 'M').Sum(obj => obj.Salary);
+
+                    Console.WriteLine("Email of people whose salary is more than " + limit.ToString("F2", CultureInfo.InvariantCulture));
+                    foreach(string email in emails)
+                    {
+                        Console.WriteLine(email);
+                    }
+
+                    Console.WriteLine("Sum of salary of people whose name starts whit 'M': " + sum.ToString("F2", CultureInfo.InvariantCulture));
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
